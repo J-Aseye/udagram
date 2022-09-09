773b7d2ba2742084c58df682a4940b802fe83ae2
J-Aseye <aseyegbadago@gmail.com>
2022-09-09 10:21:14 +0000

    Pushing files for my udagram

---
+++ set_env.sh
@@ -0,0 +1,12 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=formyudagramdev
+#export POSTGRES_HOST=postgres.cf1rugss9okp.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=localhost
+export POSTGRES_DB=postgres
+export AWS_BUCKET=fromyudagram-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
