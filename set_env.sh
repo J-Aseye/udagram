c4e38fec05b1bab3bcfa7ceb636d51df281c6edc
J-Aseye <aseyegbadago@gmail.com>
2022-09-12 14:51:36 +0000

    Building my app as a whole

--- set_env.sh
+++ set_env.sh
@@ -2,8 +2,8 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=formyudagramdev
-#export POSTGRES_HOST=postgres.cf1rugss9okp.us-east-1.rds.amazonaws.com
-export POSTGRES_HOST=localhost
+export POSTGRES_HOST=postgres.cf1rugss9okp.us-east-1.rds.amazonaws.com
+#export POSTGRES_HOST=localhost
 export POSTGRES_DB=postgres
 export AWS_BUCKET=fromyudagram-dev
 export AWS_REGION=us-east-1
